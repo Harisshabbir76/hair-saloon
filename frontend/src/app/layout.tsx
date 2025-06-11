88c921511526e0d7f5ee238b5b457652a68dd5ab
Harisshabbir76 <05harisshabbir@gmail.com>
2025-06-10 17:11:13 -0700

    initial commit

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,9 @@
 // app/layout.tsx
+"use client"; // <-- Add this directive
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import { usePathname } from 'next/navigation';
 
-// Define routes where Navbar & Footer should be hidden
 const HIDE_NAV_FOOTER_PATHS = ['/login', '/signup', '/not-found'];
 
 export default function RootLayout({
